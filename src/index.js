1bfa5da97028ae538e8ca323d2624626603c44dc
Thrasos06 <thrasos.vito@gmail.com>
2023-06-14 15:03:00 +0300

    Added Cart icon and cart dropdown div

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.styles.scss";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { ProductProvider } from "./contexts/porduct.context";
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { Outlet, Link } from "react-router-dom";
 import { Fragment, useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
@@ -29,7 +31,11 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <Link>
+            <CartIcon />
+          </Link>
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
